5104154b948eef11a9209530afaa7dbe98d27dc3
Murat <muratsat@users.noreply.github.com>
2023-05-16 13:41:27 +0600

    added hero-section

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,26 @@
-import './style.css'
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } 
+    else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((element) => {
+  observer.observe(element);
+});
+
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector(".menu");
+menuButton?.addEventListener('click', () => {
+  menuButton.classList.toggle('open');
+  menu?.classList.toggle('open');
+});
 
